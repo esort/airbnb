2003b6fa9fdae149f0d3f05e638f34f967e91e82
sort.elisabeth <sort.elisabeth@gmail.com>
2018-08-17 14:14:42 -0400

    Cration des vues photo

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,8 @@ class RoomsController < ApplicationController
 before_action :set_room, only: [:show, :edit, :update]  
 # precision, vous n avez pas besoin d etre authentifie pour voir les annonces
 before_action :authenticate_user!, except: [:show]
+ # protection de la page. elle n'est modifiable que par le propriétaire de l annonce
+  before_action :require_same_user, only: [:edit, :update] 
   
 def index
     @rooms = current_user.rooms 
@@ -14,17 +16,25 @@ end
 
 def create
       @room = current_user.rooms.build(room_params)
-      if @room.save 
-            redirect_to @room, notice:"Votre annonce a été ajouté avec succès" 
+      if @room.save
+        if params[:images] 
+       params[:images].each do |i| 
+            @room.photos.create(image: i) 
+       end 
+ end
+  @photos = @room.photos
+ redirect_to edit_room_path(@room), notice:"Votre logement a été ajouté" 
       else 
            render :new
       end 
  end
  
 def show
+  @photos = @room.photos
 end 
   
 def edit
+ @photos = @room.photos 
 end 
   
 def update
@@ -41,11 +51,18 @@ private
       end  
 
 
-def room_params 
+      def room_params 
       params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, 
                                    :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, 
                                    :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, 
                                    :active)
- end
+      end
+
+      def require_same_user 
+        if current_user.id != @room.user_id
+           flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+            redirect_to root_path
+        end
+      end
 
  end
